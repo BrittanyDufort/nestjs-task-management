dc0a081e61a252160107b1972a5b46f00f2cd154
Brittany Dufort <Brittany.Dufort@gmail.com>
2020-07-07 19:27:47 -0700

    Set `AuthGuard` on Task Controller

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -17,8 +17,11 @@ import { CreateTaskDto } from './dto/create-task.dto';
 import { GetTasksFilterDto } from './dto/get-tasks-filter.dto';
 import { TaskStatusValidationPipe } from './pipes/task-status-validation.pipe';
 import { Task } from './task.entity';
+import { UseGuards } from '@nestjs/common';
+import { AuthGuard } from '@nestjs/passport';
 
 @Controller('tasks')
+@UseGuards(AuthGuard())
 export class TasksController {
   constructor(private tasksService: TasksService) {}
 
--- src/tasks/tasks.module.ts
+++ src/tasks/tasks.module.ts
@@ -1,11 +1,13 @@
 import { Module } from '@nestjs/common';
-import { TasksController } from './tasks.controller';
-import { TasksService } from './tasks.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
+
+import { AuthModule } from '../auth/auth.module';
+import { TasksController } from './tasks.controller';
 import { TaskRepository } from './task.repository';
+import { TasksService } from './tasks.service';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([TaskRepository])],
+  imports: [TypeOrmModule.forFeature([TaskRepository]), AuthModule],
   controllers: [TasksController],
   providers: [TasksService],
 })
